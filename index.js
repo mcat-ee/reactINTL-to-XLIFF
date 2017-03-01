14c82f82032a6b9a57afcfe6d4272d9f345bc5a8
Tom <tom@enabled.com.au>
2017-03-01 12:04:15 +1030

    Initial commit; adding initial package.json and index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+var fs = require('fs');
+
+function readFileToArray(filename) {
+
+}
+
+function createBlankXLIFFFile(sourceLanguage,targetLanguage,reactIntlLines) {
+  //Create root object
+  var root = builder.create(xliff);
+  //TODO: attributes
+
+  var body = root.ele('body');
+
+  for ( var lineIndex = 0 ; lineIndex < reactIntlLines.length ; lineIndex++ ) {
+    var currentLine = reactIntlLines( lineIndex );
+
+    //Process line
+    var lineProperties = processLine(currentLine);
+    var line = body.ele('trans-unit');
+    line.att('id', lineProperties.id);
+
+    line.ele('source');
+    
+    line.ele('target');
+
+  }
+}
+
+function processLine(line) {
+
+}
